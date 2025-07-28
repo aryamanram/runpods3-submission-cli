b9bd1ba01d68ba3dfc76a93ef57bb912b7df14b0
Aryaman Ramchandran <aryamanramchandran@gmail.com>
2025-07-28 00:04:44 -0500

    cleanse

--- src/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-AWS_REGION      = os.getenv("AWS_REGION", "us-east-1")
-S3_BUCKET       = os.getenv("JOB_BUCKET",  "job-artifacts")
-S3_ENDPOINT_URL = os.getenv("S3_ENDPOINT_URL")  # leave None for real AWS
